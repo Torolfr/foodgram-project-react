5231ea5c3900dbbabf057efc7f289c2b1e1cce24
Sergey <torolfr@yandex.ru>
2022-03-20 01:58:56 +0300

    Correct filter

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -26,7 +26,7 @@ class RecipeFilter(FilterSet):
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
 
