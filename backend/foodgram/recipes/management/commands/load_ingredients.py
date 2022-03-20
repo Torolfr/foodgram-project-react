079f7d21f06e29b2cc2caba08c6e73fb604d8270
Sergey <torolfr@yandex.ru>
2022-03-20 19:37:23 +0300

    data-recipes

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Загружает таблицу ингредиентов из csv-файла'
 
     def handle(self, *args, **options):
-        with open('../../data/ingredients.csv', encoding='utf-8') as file:
+        with open('recipes/data/ingredients.csv', encoding='utf-8') as file:
             data_reader = csv.reader(file)
             for row in data_reader:
                 name, unit = row
