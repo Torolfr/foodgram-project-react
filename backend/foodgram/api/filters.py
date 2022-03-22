fcd55bcb77ef0a8297481cef857219d857ba5ef3
Sergey <torolfr@yandex.ru>
2022-03-22 21:56:21 +0300

    correct Recipefilter

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -10,8 +10,8 @@ User = get_user_model()
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
         method='get_is_in_shopping_cart'
     )
 
